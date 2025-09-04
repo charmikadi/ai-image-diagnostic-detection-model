179f5865027161192b57bfb758da2c5acf4df5f1
Charmi Kadi <68164274+charmikadi@users.noreply.github.com>
2025-09-03 22:38:15 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,9 @@
+from pathlib import Path
+import torch
+
+# Path setup
+base_dir = Path('/content/drive/My Drive/brain-images')
+train_dir = base_dir / 'Processed' / 'Training'
+test_dir = base_dir / 'Processed' / 'Testing'
+model_save_path = Path('/content/model.pth')
 
